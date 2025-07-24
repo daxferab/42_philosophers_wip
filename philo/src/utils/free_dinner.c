8d94a6f791c014a34937ec0850fbd2e1847b9b48
daxferab <daxferab.19@gmail.com>
2025-07-24 10:19:52 +0200

    Fix: free function

--- philo/src/utils/free_dinner.c
+++ philo/src/utils/free_dinner.c
@@ -6,7 +6,7 @@
 /*   By: daxferna <daxferna@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/02 20:13:10 by daxferna          #+#    #+#             */
-/*   Updated: 2025/07/21 21:59:12 by daxferna         ###   ########.fr       */
+/*   Updated: 2025/07/24 10:19:15 by daxferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,21 @@
 
 void	free_dinner(t_dinner *dinner, bool destroy)
 {
+	int	i;
+
+	i = 0;
 	if (destroy)
 	{
 		safe_mutex(&dinner->sim_start_mtx, DESTROY);
 		safe_mutex(&dinner->end_mtx, DESTROY);
 		safe_mutex(&dinner->satisfied_mtx, DESTROY);
 		safe_mutex(&dinner->last_meal_mtx, DESTROY);
+		safe_mutex(&dinner->print_mtx, DESTROY);
+		while (i < dinner->philos_nbr)
+		{
+			safe_mutex(&dinner->philos[i].right_fork->fork_id, DESTROY);
+			i++;
+		}
 	}
 	if (dinner->philos)
 		free(dinner->philos);
